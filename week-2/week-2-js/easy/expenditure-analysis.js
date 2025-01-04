4503a3c3818def46898ed7884edc214548afd9cf
Kiran Kumar Rega <57248976+kiranrega@users.noreply.github.com>
2025-01-04 20:24:26 +0530

    Update expenditure-analysis.js

--- week-2/week-2-js/easy/expenditure-analysis.js
+++ week-2/week-2-js/easy/expenditure-analysis.js
@@ -13,8 +13,39 @@
   Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
 */
 
+
+// function calculateTotalSpentByCategory(transactions) {
+//   let re = []
+//   elementTr = {}
+//   transactions.forEach((el) => {
+//     let category = el.category
+//     if (elementTr[category]) {
+//       re.push({category: el.category, totalSpent: el.price + elementTr["total"]})
+//     } else {
+//       elementTr[category] = true
+//       elementTr.total = el.price
+//     }
+//   })
+//   return re;
+// }
+
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  let trackerMap = new Map()
+  let category;
+  transactions.forEach((transaction) => {
+    category =  transaction.category
+      if (trackerMap.has(category)) {
+        trackerMap.set(category, trackerMap.get(category) + transaction.price )
+      } else {
+        trackerMap.set(category, transaction.price)
+      }
+  })
+  let res = []
+  for (let i = 0; i< trackerMap.size; i++) {
+    res.push({category, totalSpent: trackerMap.get(category)})
+  }
+  return res
 }
+
 
 module.exports = calculateTotalSpentByCategory;
